973dd6d67ffb20e1f39fdcc64adfd5c74a39e64d
Matt Butler <mattwbutler@gmail.com>
2018-07-15 18:19:32 -0700

    ESLint airbnb-config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>mattbutler.io</h2>
+    </div>
+  </div>
+);
+
+export default App;
