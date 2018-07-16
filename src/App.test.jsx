540921b980aa355406623680f4149d662d63fca7
Matt Butler <mattwbutler@gmail.com>
2018-07-15 19:02:51 -0700

    Add enzyme for testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('renders correctly', () => {
+  const wrapper = shallow(<App />);
+
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
